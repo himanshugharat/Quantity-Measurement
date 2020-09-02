e30074c93d3056962f06f7b3a20d05133b8e9659
unknown <himanshugharat44@gmail.com>
2020-09-02 00:23:59 -0500

    REFACTORED: test case for the negative case possible

--- src/test/java/com/bridglabz/MeasurementTest.java
+++ src/test/java/com/bridglabz/MeasurementTest.java
@@ -11,12 +11,17 @@ public class MeasurementTest {
     public void setup() {
         quantityCalculator = new QuantityCalculator();
     }
-
     @Test
-    public void givenLength_whenZeroFeet_ShouldResultTrue() {
-        quantityCalculator = new QuantityCalculator();
-        boolean compare = quantityCalculator.compareLengths(new Feet(0), new Feet(0));
-        Assert.assertTrue(compare);
+    public void given0FeetAnd0FeetLength_whenEqual_ShouldReturnTrue() {
+        Feet feet = new Feet(0.0);
+        Feet feet1 = new Feet(0.0);
+        Assert.assertEquals(feet,feet1);
+    }
+    @Test
+    public void given0FeetAnd1FeetLength_whenNotEqual_ShouldReturnFalse() {
+        Feet feet = new Feet(0.0);
+        Feet feet1 = new Feet(1.0);
+        Assert.assertNotEquals(feet,feet1);
     }
 
     @Test
@@ -47,6 +52,18 @@ public class MeasurementTest {
         quantityCalculator = new QuantityCalculator();
         boolean compare = quantityCalculator.compareLengths(new Feet(10), new Feet(10));
         Assert.assertTrue(compare);
+    }
+    @Test
+    public void given0InchAnd0InchLength_whenEqual_ShouldReturnTrue() {
+        Inch inch = new Inch(0.0);
+        Inch inch1 = new Inch(0.0);
+        Assert.assertEquals(inch,inch1);
+    }
+    @Test
+    public void given0InchAnd1InchLength_whenNotEqual_ShouldReturnFalse() {
+        Inch inch = new Inch(0.0);
+        Inch inch1 = new Inch(1.0);
+        Assert.assertNotEquals(inch,inch1);
     }
 
     @Test
